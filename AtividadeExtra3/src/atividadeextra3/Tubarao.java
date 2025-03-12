7bdf348658b49b8e42dfbab40a83adbafd56f8a3
Klint Burzlaff Berta Lemes <165198113+KlintBzl@users.noreply.github.com>
2025-03-12 14:57:53 -0300

    Reajuste Tubarao

--- AtividadeExtra3/src/atividadeextra3/Tubarao.java
+++ AtividadeExtra3/src/atividadeextra3/Tubarao.java
@@ -1,16 +1,23 @@
-
 package atividadeextra3;
 
 public class Tubarao extends AnimalMarinho{
-    
     private int dentes;
+
+    public Tubarao(String habitat) {
+        super(habitat);
+    }
     
     public void definirDentes(int dentes){
         this.dentes = dentes;
     }
+    public void exibirInfo(){
+        System.out.println("Habitat do tubarão: " + habitat);
+        System.out.println("Dentes: " + dentes);
+        System.out.println("=========================");
+    }
     @Override
     public void fazerSom() {
         System.out.println("O tubarão não faz som... mas ele se comunica zigue-zagueando");
+        System.out.println("----------------------------------------");
     }
-    
 }
