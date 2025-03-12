54d24b3e3b20dfae0e457f366a7a3b23c022ac7a
Klint Burzlaff Berta Lemes <165198113+KlintBzl@users.noreply.github.com>
2025-03-12 14:57:04 -0300

    Reajuste da classe pai

--- AtividadeExtra3/src/atividadeextra3/AnimalMarinho.java
+++ AtividadeExtra3/src/atividadeextra3/AnimalMarinho.java
@@ -4,5 +4,9 @@ package atividadeextra3;
 public abstract class AnimalMarinho {
     protected String habitat;
     
+    public AnimalMarinho(String habitat){
+        this.habitat = habitat;
+    }
+    
     public abstract void fazerSom();
 }
